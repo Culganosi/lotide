da9d2ff0079031a3947e434b6b4c0ccd2101d886
Culganosi <culgan_@hotmail.com>
2022-03-07 18:00:31 -0500

    assertEqual exercise pushed

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔✔✔Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 24);
+assertEqual("Bob", "Loblaw");
+assertEqual(24.25, 24.25);
+
+
+
